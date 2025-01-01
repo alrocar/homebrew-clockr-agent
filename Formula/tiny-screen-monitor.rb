6ee766c1a98db3f91f977336e09d2735276dfe52
alrocar <alrocar@tinybird.co>
2025-01-01 21:47:28 +0100

    update formula

--- Formula/tiny-screen-monitor.rb
+++ Formula/tiny-screen-monitor.rb
@@ -5,7 +5,7 @@ class TinyScreenMonitor < Formula
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
-  depends_on "curl" => ">=8.4"
+  depends_on "curl"
 
   def install
     bin.install "bin/tiny-screen-monitor.sh" => "tiny-screen-monitor"
