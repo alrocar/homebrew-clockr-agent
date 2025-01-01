66dd58e016b56451df159e5be4fd483bc62aac68
alrocar <alrocar@tinybird.co>
2025-01-01 21:43:34 +0100

    update formula

--- Formula/tiny-screen-monitor.rb
+++ Formula/tiny-screen-monitor.rb
@@ -1,7 +1,7 @@
 class TinyScreenMonitor < Formula
   desc "Monitor screen lock status and active applications on macOS"
-  homepage "https://github.com/alrocar/tiny-screen-monitor"
-  url "https://github.com/alrocar/tiny-screen-monitor/archive/refs/tags/v0.0.0.dev0.tar.gz"
+  homepage "https://github.com/alrocar/homebrew-tiny-screen-monitor"
+  url "https://github.com/alrocar/homebrew-tiny-screen-monitor/archive/refs/tags/v0.0.0.dev1.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
